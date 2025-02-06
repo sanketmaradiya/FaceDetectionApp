2f760a9d5a94ebfce86c6f6145b3564f10852147
fdevgimmegift <fdev1@personalx.com>
2025-02-06 11:42:12 +0530

    Initial Commit

---
+++ FaceDetectionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceDetectionApp
+//
+//  Created by Nimap on 06/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
